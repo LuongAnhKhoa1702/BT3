703d0c330b8f3be49acb5430b7233f7bf51ce5c9
LuongAnhKhoa1702 <luongkhoa0175@gmail.com>
2025-05-15 17:17:05 +0700

    Initial commit on main branch

--- obj/Debug/net8.0/Buoi1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Buoi1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Buoi1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f62046308102601b59439467239d2f8979263a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Buoi1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Buoi1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
